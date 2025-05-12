875c10ee16eecc90aaef1b3cdba9c2d13217ebba
TysikGG <avdej.vitalij@gmail.com>
2025-05-12 22:13:35 +0300

    Update database.ts

--- services/src/database/mongodb/database.ts
+++ services/src/database/mongodb/database.ts
@@ -1,13 +1,22 @@
 import mongoose from 'mongoose';
+import fastify from 'fastify';
 
-class Database {
+export default class MongoDB {
     mongodb_token: string
 
     constructor(mongodb_token: string) {
         this.mongodb_token = mongodb_token;
     }
 
-    init() {
+    async init() {
+        return await mongoose.connect(this.mongodb_token);
+    }
 
+    async connect_API() {
+        const server = fastify();
+
+        server.listen({ port: 27017, host: "0.0.0.0" }, (err, address) => {
+            console.log(`База данных подключена, адрес: ${address}`)
+        })
     }
 }
