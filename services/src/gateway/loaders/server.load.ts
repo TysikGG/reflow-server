1b01843d8628938994e77a3152dad4968f77539b
TysikGG <avdej.vitalij@gmail.com>
2025-05-12 22:13:09 +0300

    Update server.load.ts

--- services/src/gateway/loaders/server.load.ts
+++ services/src/gateway/loaders/server.load.ts
@@ -1,15 +1,18 @@
 import fastify from 'fastify'
+import loadDatabase from '../../database/Load';
 
 export default class Server {
     load() {
         const server = fastify();
 
-        server.get('/test', async (request, reply) => {
-            return 'test'
-        })
+        server.get('/test', async (request, reply) => {return 'test'})
         
         server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
             console.log(`Сервер запущен, адрес: ${address}`)
         })
+    }
+
+    connect_database() {
+        loadDatabase();
     }
 }
