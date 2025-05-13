ea599fe126dffee27bedb3c32896c5420da7d664
TysikGG <avdej.vitalij@gmail.com>
2025-05-13 22:29:41 +0300

    Update server.load.ts

--- services/src/gateway/loaders/server.load.ts
+++ services/src/gateway/loaders/server.load.ts
@@ -5,8 +5,8 @@ export default class Server {
     load() {
         const server = fastify();
 
-        server.get('/test', async (request, reply) => {return 'test'})
-        
+        server.get('/test', async () => { return 'test' })
+
         server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
             console.log(`Сервер запущен, адрес: ${address}`)
         })
