b8d544b32d45c2b0befc58203d9b56fe227d4027
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-14 23:47:01 +0300

    Update API.load.ts

--- services/src/database/mongodb/handlers/API.load.ts
+++ services/src/database/mongodb/handlers/API.load.ts
@@ -5,6 +5,6 @@ import path from "path";
 export default function load_API(server: FastifyInstance) {
     server.register(autoload, {
         dir: path.join(__dirname, '..', 'API'),
-        options: { prefix: process.env.DATABASE_ROUTE_SECRET + 'database' }
+        options: { prefix: process.env.DATABASE_ROUTE_SECRET + '/database' }
     }).then(() => console.log("Маршруты базы данных инициализированы:\n" + server.printRoutes()));
 }
