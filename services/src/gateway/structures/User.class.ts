37ccaf318b7fe8d3310f89b47f3c0871fa34db9e
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-14 23:47:19 +0300

    Update User.class.ts

--- services/src/gateway/structures/User.class.ts
+++ services/src/gateway/structures/User.class.ts
@@ -1,8 +1,20 @@
 import User from "../../libs/structures/User";
+import database from "./Database.class";
 
-export default class GatewayUser implements User {
-    id: string;
-    data?: object;
-    auth?: UserAuthProps;
-    
+export default class GatewayUser extends User {
+    async register(hashed_password: string) {
+        const username = this.auth.username;
+
+        if (!username || !hashed_password) return new Error("Никнейм или пароль не указаны при регистрации!");
+
+        await database.createUser(this, hashed_password);
+    }
+
+    async login(hashed_password: string) {
+        const username = this.auth.username;
+
+        if (!username || !hashed_password) return new Error("Никнейм или пароль не указаны при входе!");
+
+        await database.getUser(this, hashed_password);
+    }
 }
