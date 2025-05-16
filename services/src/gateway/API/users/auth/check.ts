18138337f9b0111f72a9ab428f0faa3e163c3cfd
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-16 15:25:47 +0300

    Create check.ts

---
+++ services/src/gateway/API/users/auth/check.ts
@@ -0,0 +1,17 @@
+import { FastifyPluginAsync } from 'fastify';
+import GatewayUser from '../../../structures/User.class';
+
+const loginRoute: FastifyPluginAsync = async (fastify, options) => {
+    fastify.post('/check', async (req, res) => {
+        const username = req.body["username"];
+        if (!username) return { error: true, status: "INCORRECT_REQUEST" };
+
+        const user = new GatewayUser();
+
+        const data = await user.checkUsername(username);
+        console.log(data);
+        return data;
+    })
+};
+
+export default loginRoute;
