d51263b33e3a675ef7845033937700085477ab09
TysikGG <avdej.vitalij@gmail.com>
2025-05-17 22:21:45 +0300

    Create login.ts

---
+++ services/src/database/mongodb/API/users/login.ts
@@ -0,0 +1,23 @@
+import { FastifyPluginAsync } from 'fastify';
+import MongoUser from '../../structures/User.class';
+
+const databaseGetRoute: FastifyPluginAsync = async (fastify, options) => {
+    fastify.post('/login', async (req, res) => {
+        try {
+            const user = new MongoUser();
+            const username = req.body["username"];
+
+            if (username) {
+                const data = await user.findByUsername(username);
+                
+            } else {    
+                res.status(404);
+                return { error: true, status: "NOT_FOUND" }
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    });
+};
+
+export default databaseGetRoute;
