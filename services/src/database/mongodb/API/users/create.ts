5bea8cd81c3a69223ac10db0f32f3ef268f54c6d
TysikGG <avdej.vitalij@gmail.com>
2025-05-17 22:21:10 +0300

    Update create.ts

--- services/src/database/mongodb/API/users/create.ts
+++ services/src/database/mongodb/API/users/create.ts
@@ -5,12 +5,16 @@ const databaseCreateRoute: FastifyPluginAsync = async (fastify, options) => {
     fastify.post('/create', async (req, res) => {
         try {
             const user = new MongoUser();
+
             const mongoUser = Object.assign(user, req.body["user"]) as MongoUser;
+
             mongoUser.create(req.body["hashed_password"]);
+
             res.status(201);
             return { success: true };
         } catch (e) {
             console.log(e);
+            
             res.status(500);
             return { error: true };
         }
