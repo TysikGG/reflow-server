79d4b601cf5ca7b399392f01948a9178144e5e34
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-20 15:32:05 +0300

    Update User.class.ts

--- services/src/gateway/structures/User.class.ts
+++ services/src/gateway/structures/User.class.ts
@@ -1,19 +1,24 @@
+import ServerError from "../../libs/functions/error";
 import User from "../../libs/structures/User";
 import database from "./Database.class";
 
 export default class GatewayUser extends User {
     async register(hashed_password: string) {
         const username = this.data.username;
+        if (!username || !hashed_password) return new ServerError("USERNAME_NOT_FOUND");
 
-        if (!username || !hashed_password) return new Error("Никнейм или пароль не указаны при регистрации!");
+        const reg = new RegExp("^[a-z0-9]*$");
+        if (!username.match(reg)) return new ServerError("INCORRECT_USERNAME_SYMBOLS");
+
+        const checked = await this.checkUsername(username);
+        if (checked.found) return new ServerError("USERNAME_ALREADY_EXIST");
 
         await database.createUser(this, hashed_password);
     }
 
     async login(hashed_password: string) {
         const username = this.data.username;
-
-        if (!username || !hashed_password) return new Error("Никнейм или пароль не указаны при входе!");
+        if (!username || !hashed_password) return new ServerError("USERNAME_NOT_FOUND");
 
         const user = await database.login(username, hashed_password);
 
@@ -21,10 +26,11 @@ export default class GatewayUser extends User {
     }
 
     async checkUsername(username: string) {
-        if (!username) return new Error("Никнейм не был указан для функции checkUsername!");
-
+        if (!username) return new ServerError("INCORRECT_FORM");
         const usernameCheck = await database.checkUsername(username);
 
         return usernameCheck;
     }
+
+    setUsername(username: string) { this.data.username = username };
 }
